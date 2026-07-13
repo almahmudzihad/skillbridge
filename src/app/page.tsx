31fc7a6f537a07a8227bc5823414544708a7360c
almahmudzihad <amzihad@gmail.com>
2026-07-13 08:46:59 +0600

    some text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div>
       <h1>Hello World</h1>
-      <p>This is a paragraph</p>
+      <p className="text-3xl">This is a paragraph</p>
      
     </div>
   );
